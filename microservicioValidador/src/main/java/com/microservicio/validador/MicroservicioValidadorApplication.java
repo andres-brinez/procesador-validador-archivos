2dfbfdc3f77b63d24abbff8d837807fe8748550e
andres briñez <brinezlopez08@gmail.com>
2023-08-14 12:18:56 -0500

    Crear microservicio

---
+++ microservicioValidador/src/main/java/com/microservicio/validador/MicroservicioValidadorApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.validador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioValidadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioValidadorApplication.class, args);
+	}
+
+}
