383fc1733a083c982313f80c6816557442f7fdb0
andres briñez <brinezlopez08@gmail.com>
2023-08-14 20:18:36 -0500

    Crear todas las validaciones para csv

--- microservicioValidador/src/main/java/com/microservicio/validador/Service.java
+++ microservicioValidador/src/main/java/com/microservicio/validador/Service.java
@@ -13,30 +13,42 @@ public class Service {
         if (registro.get(0).equals("Index")) {
 
             System.out.println("Cabecera del archivo");
+            respuesta= false;
 
         }
         else{
-
-            System.out.println("Registro: "+ registro);
 
             // Estructura registro [Index, User Id, First Name, Last Name, Sex, Email, Phone, Date of birth, Job Title]
 
             String email = registro.get(5);
             String date = registro.get(7);
+            String jobTitle = registro.get(8);
+            String[] jobTitles = {"Haematologist", "Phytotherapist", "Building surveyor", "Insurance account manager", "Educational psychologist"};
 
             LocalDate dateFormat = LocalDate.parse(date); // pasar el string a formato date
 
-
             // Validar email que tenga la estructura correcta
-            if (!email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            if (!email.matches("^[A-Za-z0-9]+@(.+)$")) {
+
+                System.out.println("ID: " + registro.get(0) + " |  Email no valido " + email );
                 respuesta= false;
 
             }
             // Validar Date of Birth que sea mayor a 1980
             else if(dateFormat.isBefore(LocalDate.of(1980, 1, 1))) {
+
+                System.out.println("ID: " + registro.get(0) + " |  Fecha no valida " + date );
                 return false;
             }
 
+            boolean isValidJobTitle = false;
+            for (String title : jobTitles) { // recorrer el arreglo de jobTitles
+                if (title.equals(jobTitle)) { // si el jobTitle es igual a alguno de los jobTitles del arreglo
+                    isValidJobTitle = true;
+                    break;
+                }
+                respuesta= isValidJobTitle;
+            }
 
         }
 
